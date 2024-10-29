f216d21982261863facbb66edb05e350ac5dd1f2
Juan Cabello <jcabelloc@itana.pe>
2024-10-28 20:46:10 -0500

    Implementacion de Gestion de Sesiones

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
+const session = require('express-session');
+
 
 
 const adminRoutes = require('./routes/admin')
@@ -19,8 +21,9 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'algo muy secreto', resave: false, saveUninitialized: false }));
+
 
 
 app.use((req, res, next) => {
